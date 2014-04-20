3b3d7fb659e8b37e1bf6a2826aead9216fa8cfcf
Mario Castro <mariocaster@gmail.com>
2014-04-20 19:39:26 +0200

    Current aws machines of masters and slaves configured

--- conf.aws/hadoop-env.sh
+++ conf.aws/hadoop-env.sh
@@ -53,7 +53,7 @@ export HADOOP_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_JOBTRACKER
 # The scheduling priority for daemon processes.  See 'man nice'.
 # export HADOOP_NICENESS=10
 export HADOOP_OPTS=-Djava.net.preferIPv4Stack=true
-export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-1.*
 export HADOOP_INSTALL=/var/hadoop
 export PATH=$PATH:$HADOOP_INSTALL/bin:$HADOOP_INSTALL/sbin
 export HADOOP_PREFIX=/var/hadoop
